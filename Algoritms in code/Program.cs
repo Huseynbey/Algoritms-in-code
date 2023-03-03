f89d6ef366b8be5f82eac1c6e1ca6eec7ba54379
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-03-03 20:28:13 +0400

    Add project files.

---
+++ Algoritms in code/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Algoritms_in_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1:
+
+            //int number = 76;
+            //int rem = 0;
+            //int sum = 0;
+            //while (number > 0)
+            //{
+            //    rem = number % 10;
+            //    sum = sum + rem;
+            //    number = number / 10;
+
+            //}
+            //if (sum % 2 == 0)
+            //{
+            //    Console.WriteLine("eded cutdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded tekdir");
+            //}
+
+            //Q2: 
+
+            //int number = 100;
+            //int sum = 0;
+
+            //for (int i = 100; i > 0; i--)
+            //{
+            //    if (i % 13 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //        sum = sum + i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+            //Q3:
+
+            //int number = 654;
+            //int rem = 0;
+            //int min = number % 10;
+            //while (number > 0)
+            //{
+            //    rem = number % 10;
+            //    if (rem <= min)
+            //    {
+            //        min = rem;
+
+            //    }
+            //    number /= 10;
+            //}
+
+            //Console.WriteLine(min);
+        }
+    }
+}
